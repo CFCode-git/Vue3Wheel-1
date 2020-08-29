cdddfc400879f6f0c9319567d7cb257812569a09
CFCode-git <435184766@qq.com>
2020-08-29 16:18:19 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <div>ojbk</div>
+    <div>ojbk</div>
+  </p>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
